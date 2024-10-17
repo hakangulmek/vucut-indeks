cd38e16c44426df1e7080777cd0b15791ee1c505
Hakan Gülmek <gulmekhakan6@gmail.com>
2024-10-17 18:20:07 +0300

    First Commit

---
+++ src/App.jsx
@@ -0,0 +1,11 @@
+import MainPage from "./Pages/MainPage/mainPage.tsx";
+import "./index.css";
+function App() {
+  return (
+    <div className="h-[729px] bg-[#0B192C]">
+      <MainPage />
+    </div>
+  );
+}
+
+export default App;
